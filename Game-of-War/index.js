bf320b8dbf155210a5e19b755dac191b1878f1fd
famanakis <73799603+famanakis@users.noreply.github.com>
2023-02-20 22:41:02 -0700

    commit

--- Game-of-War/index.js
+++ Game-of-War/index.js
@@ -5,10 +5,13 @@ const cardContainer = document.getElementById('card-container')
 const winnerEl = document.getElementById('winner-el')
 const playerEl = document.getElementById('player-el')
 const computerEl = document.getElementById('computer-el')
-
+const roundsEl = document.getElementById('rounds-left-el')
 
 //Variables
 let deckId
+let playerCount = 0
+let computerCount = 0
+let draws = 0
 
 //Functions
 function getNewDeck() {
@@ -16,7 +19,9 @@ function getNewDeck() {
     .then (res => res.json())
     .then (data => {
         deckId = data.deck_id
-        console.log(deckId)
+        btnDraw.disabled = false
+        cardContainer.innerHTML = `<h2>Draw Cards</h2>`
+        roundsEl.innerHTML = ''
     })
 }
 
@@ -24,20 +29,53 @@ function drawCards() {
     fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then (res => res.json())
     .then( data => {
-        console.log(data.cards)
         const cards = data.cards.map(card => 
             `<img class="card" src="${card.image}"></img>`
-            )
+            ).join('')
         cardContainer.innerHTML= cards
         const cardOneValue = data.cards[0].value
         const cardTwoValue = data.cards[1].value
-        console.log(cardOneValue)
-        console.log(cardTwoValue)
+        roundWinner(cardOneValue, cardTwoValue)
+        draws ++
+        roundsEl.innerHTML = `<p>Draw: ${draws}/26</p>`
+        if(data.remaining === 0) {endGame(playerCount, computerCount)}
     })
 }
 
-function roundWinner() {
+function endGame(player, computer) {
+    player > computer ? winnerEl.innerHTML = `<h1>Player wins!</h1>` :
+    computer > player ? winnerEl.innerHTML = `<h1>Computer wins!</h1>` :
+    winnerEl.innerHTML = `<h1>Tie!<h1>`
+    roundsEl.innerHTML = ''
+    cardContainer.innerHTML = ''
+    btnDraw.disabled = true
+    const fireworks = new Fireworks.default(cardContainer)
+    fireworks.start()
+    setTimeout(newGame, 12000)   
+}
 
+function newGame() {
+    playerCount = 0
+    computerCount = 0
+    winnerEl.innerHTML = `<h1>Game of War</h1>`
+    cardContainer.innerHTML = `<button type="button" id="new-deck" class="btn btn-new-deck">Draw a New Deck</button>`
+    playerEl.textContent = "Player: " + playerCount
+    computerEl.textContent = "Computer: " + computerCount
+    btnNewDeck.style.display = 'visible'
+}
+
+function roundWinner(player, computer) {
+    const cardArr = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    const playerValue = cardArr.indexOf(player)
+    const computerValue = cardArr.indexOf(computer)
+    if( playerValue === computerValue) {
+    } else if(playerValue > computerValue) {
+        playerCount ++
+    } else {
+        computerCount ++
+    } 
+    playerEl.textContent = "Player: " + playerCount
+    computerEl.textContent = "Computer " + computerCount
 }
 
 //Event Listeners
